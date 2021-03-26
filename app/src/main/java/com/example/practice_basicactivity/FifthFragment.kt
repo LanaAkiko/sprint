c5cf4d43b94e7614240a42f43d99d4c31284bc7a
Lana Anderson <lanaakiko@gmail.com>
2021-03-26 13:50:12 -0400

    Jiaoli!

--- app/src/main/java/com/example/practice_basicactivity/FifthFragment.kt
+++ app/src/main/java/com/example/practice_basicactivity/FifthFragment.kt
@@ -10,16 +10,17 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FifthFragment : Fragment() {
 
-    /*var starttime = "000000000000000000000000000000000000000000000000"
-    var mstart = starttime.substring(14, 16)
-    var sstart = starttime.substring(17, 19)*/
-    var timeSelected = 0
+class Timer : Fragment() {
+    private var running = false
+    private var minutes = 0
+    private var seconds = 0
+    private var timeSelected = 0
+    private var secRemaining = 0.toLong()
+    private lateinit var countdown: CountDownTimer
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -36,65 +37,74 @@ class FifthFragment : Fragment() {
             findNavController().navigate(R.id.action_FifthFragment_to_FirstFragment)
         }
 
-        view.findViewById<Button>(R.id.timer5).setOnClickListener {
-            view.findViewById<TextView>(R.id.timer).text = "Time selected: 5 minutes"
-            timeSelected = 5
-        }
-
-        view.findViewById<Button>(R.id.timer10).setOnClickListener {
-            view.findViewById<TextView>(R.id.timer).text = "Time selected: 10 minutes"
-            timeSelected = 10
-        }
-
-        view.findViewById<Button>(R.id.timer15).setOnClickListener {
-            view.findViewById<TextView>(R.id.timer).text = "Time selected: 15 minutes"
-            timeSelected = 15
-        }
-
         view.findViewById<Button>(R.id.timer_start).setOnClickListener {
-            //5 min
-            var sec = (timeSelected * 60 * 1000).toLong()
-            timer(sec)
+
+            if (seconds >= 60) {
+                minutes += seconds / 60
+                seconds %= 60
+            }
+            if (!running) {
+                running = true
+                timer(timeSelected.toLong() * 1000)
+            }
+            countdown.start()
+
         }
 
         view.findViewById<Button>(R.id.timer_pause).setOnClickListener {
+            countdown.cancel()
+            running = false
         }
 
         view.findViewById<Button>(R.id.timer_reset).setOnClickListener {
-            view.findViewById<TextView>(R.id.timer).text = "00:00"
+            countdown.cancel()
+            timeSelected = 0
+            view.findViewById<TextView>(R.id.timer)?.setText("00:00")
         }
+
+        view.findViewById<Button>(R.id.timeEnter).setOnClickListener {
+            var min = view.findViewById<TextView>(R.id.minInput).text.toString()
+            var minInt = min.toInt()
+            var sec = view.findViewById<TextView>(R.id.secInput).text.toString()
+            var secInt = sec.toInt()
+
+            if (min.toInt() < 10) {
+                min = "0" + min.toString()
+            }
+            if (sec.toInt() < 10) {
+                sec = "0" + sec.toString()
+            }
+
+            if(!running){
+                view?.findViewById<TextView>(R.id.timer)?.setText(min + ":" + sec)
+            }
+
+            timeSelected = minInt * 60 + secInt
+        }
+
     }
 
-    fun timer(time: Long){
-        object : CountDownTimer(time, 1000) {
+    fun timer(time: Long) {
+        countdown = object : CountDownTimer(time, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                var secRemaining = millisUntilFinished / 1000
+                secRemaining = millisUntilFinished / 1000
                 var min = (secRemaining / 60).toString()
                 var sec = (secRemaining - min.toInt() * 60).toString()
-                if (min.toInt() < 10){
+                if (min.toInt() < 10) {
                     min = "0" + min.toString()
                 }
-                if (sec.toInt() < 10){
+                if (sec.toInt() < 10) {
                     sec = "0" + sec.toString()
                 }
-                view?.findViewById<TextView>(R.id.timer)?.setText("Remaining time " + min + ":" + sec)
+
+                timeSelected = min.toInt() * 60 + sec.toInt()
+                view?.findViewById<TextView>(R.id.timer)?.setText(min + ":" + sec)
             }
 
             override fun onFinish() {
                 view?.findViewById<TextView>(R.id.timer)?.setText("done!")
+                running = false
             }
-        }.start()
-    }
-
-    /*fun timer(go: Boolean){
-        view.findViewById<TextView>(R.id.timer).text = "111"
-        var min = view?.findViewById<TextView>(R.id.timer)?.text?.substring(0, 2)?.toInt()
-        var sec = view?.findViewById<TextView>(R.id.timer)?.text?.substring(3, 5)?.toInt()
-        if (min != null && sec != null) {
-            min ++
-            sec ++
-            view?.findViewById<TextView>(R.id.timer)?.text = min.toString() + ":" + sec.toString()
         }
     }
-     */
 }
