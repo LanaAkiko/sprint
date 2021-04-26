017d5270dce5f348e147404510551dd2f3800cd2
Jiaoli <jiaolibow@gmail.com>
2021-04-26 09:52:32 -0400

    embeded the thing + renamed some files

--- app/src/main/java/com/example/practice_basicactivity/Grades.kt
+++ app/src/main/java/com/example/practice_basicactivity/Grades.kt
@@ -1,31 +1,46 @@
 package com.example.practice_basicactivity
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class FourthFragment : Fragment() {
+class Grades : Fragment() {
+    private lateinit var webView: WebView
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fourth, container, false)
+        return inflater.inflate(R.layout.grades, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        webView = view.findViewById<WebView>(R.id.webview)
+        webView.settings.setJavaScriptEnabled(true)
+
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.action_FourthFragment_to_FirstFragment)
         }
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+        webView.loadUrl("https://md-mcps-psv.edupoint.com/PXP2_Login_Student.aspx?regenerateSessionId=True")
+
+
     }
 }
