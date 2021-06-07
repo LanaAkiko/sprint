219d666a9fa41f0f919f67cc35a92c70a97d9b43
Lana Anderson <lanaakiko@gmail.com>
2021-06-07 17:48:54 -0400

    sam's todo list - edit the .kt file it's very wrong

--- app/src/main/java/com/example/practice_basicactivity/ToDoList.kt
+++ app/src/main/java/com/example/practice_basicactivity/ToDoList.kt
@@ -1,31 +1,66 @@
 package com.example.practice_basicactivity
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.SparseBooleanArray
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 import androidx.navigation.fragment.findNavController
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
+
 class ToDoList : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.todo_list, container, false)
-    }
+        findViewById<Button>(R.id.ButtonToHome).setOnClickListener {
+            findNavController().navigate(R.id.action_Todo_to_Home)
+        }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        // Initializing the array lists and the adapter
+        var itemlist = arrayListOf<String>()
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemlist)
 
-        view.findViewById<Button>(R.id.btn_toHome).setOnClickListener {
-            findNavController().navigate(R.id.act_listToHome)
+        // Adding the items to the list when the add button is pressed
+        findViewById<TextView>(R.id.add).setOnClickListener {
+            itemlist.add(findViewById<TextView>(R.id.editText).text.toString())
+            listView.adapter = adapter
+            adapter.notifyDataSetChanged()
+            // This is because every time when you add the item the input space or the edit text space will be cleared
+            findViewById<TextView>(R.id.editText).setText("")
+        }
+
+        // Selecting and Deleting the items from the list when the delete button is pressed
+        findViewById<TextView>(R.id.delete).setOnClickListener {
+            val position: SparseBooleanArray = listView.checkedItemPositions
+            val count = listView.count
+            var item = count - 1
+            while (item >= 0) {
+                if (position.get(item)) {
+                    adapter.remove(itemlist.get(item))
+                }
+                item--
+            }
+            position.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        // Clearing all the items in the list when the clear button is pressed
+        findViewById<TextView>(R.id.clear).setOnClickListener {
+            itemlist.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        // Adding the toast message to the list when an item on the list is pressed
+        listView.setOnItemClickListener { adapterView, view, i, l ->
+            android.widget.Toast.makeText(
+                this,
+                "You Selected the item --> " + itemlist.get(i),
+                android.widget.Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
