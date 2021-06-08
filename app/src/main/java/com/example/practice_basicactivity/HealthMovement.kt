9b3b809c9b89f80c234ff8658cd92da0a0722d29
Jiaoli <jiaolibow@gmail.com>
2021-06-07 21:43:09 -0400

    Adding Samantha's to-do, implementing storage in health (not done, many errors)

--- app/src/main/java/com/example/practice_basicactivity/HealthMovement.kt
+++ app/src/main/java/com/example/practice_basicactivity/HealthMovement.kt
@@ -1,23 +1,28 @@
 package com.example.practice_basicactivity
 
-import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-
+import android.util.Log
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class HealthMovement : Fragment() {
+    val MyPREFERENCES = "MyPrefs"
+    var sharedpreferences: SharedPreferences? = null
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.health_movement, container, false)
@@ -25,12 +30,16 @@ class HealthMovement : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedpreferences = activity?.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        val typeList = sharedpreferences?.getStringSet("healthtype", null)
+        var numList = sharedpreferences?.getString("healthnum", "")
+        val numListTemp = numList?.split(",")?.toMutableList()
+        val unitList = sharedpreferences?.getStringSet("healthunites", null)
 
         view.findViewById<Button>(R.id.button_second).setOnClickListener {
             findNavController().navigate(R.id.act_healthToHome)
         }
-
-        val goalList = arrayOfNulls<String>(21) //can only have 7 items
 
         view.findViewById<Button>(R.id.waterEnter).setOnClickListener {
             val type = view.findViewById<TextView>(R.id.healthType).text.toString()
@@ -38,31 +47,49 @@ class HealthMovement : Fragment() {
             val units = view.findViewById<TextView>(R.id.healthUnits).text.toString()
             var goalString = ""
 
-            if (units == "" && goalList.contains(type)){
-                val i = goalList.indexOf(type)
-                val newNum = (goalList.get(i+1)?.toInt()?.minus(num.toInt()))
-                goalList.set(i+1, newNum.toString())
-            }
-
-            for (i in 0..6) {
-                var a = goalList.get(3*i)
-                var b = goalList.get(3*i +1)
-                var c = goalList.get(3*i +2)
-                if (a != null && b != null && c != null) {
-                    goalString += a + " goal: " + b + " " + c + "\n"
-                }
-
-                else if (!goalList.contains(type)){
-                    goalList.set(3*i, type)
-                    goalList.set(3*i +1, num)
-                    goalList.set(3*i +2, units)
-                    goalString += type + " goal: " + num + " " + units + "\n"
+            if (typeList != null) {
+                if (units == "" && typeList.contains(type)){
+                    val i = typeList.indexOf(type)
+                    val newNum = (numList?.get(i+1)?.toInt()?.minus(num.toInt()))
+                    if (numListTemp != null) {
+                        numListTemp.set(i, newNum.toString())
+                    }
                 }
             }
+
+            if (typeList != null) {
+                for (i in 1..typeList.size) {
+                    var type = typeList?.elementAt(i)
+                    var num = numList?.get(i)
+                    var unit = unitList?.elementAt(i)
+                    if (type != null && num != null && unit != null) {
+                        goalString += type + " goal: " + num + " " + unit + "\n"
+                    } else if (typeList != null) {
+                        if (!typeList.contains(type)){
+                            if (typeList != null) {
+                                typeList.add(type)
+                            }
+                            if (numList != null) {
+                                numList += num
+                            }
+                            if (unitList != null) {
+                                unitList.add(unit)
+                            }
+                            goalString += type + " goal: " + num + " " + units + "\n"
+                        }
+                    }
+                }
+            }
+            goalString = sharedpreferences?.getStringSet("health", null)!!.elementAt(0)
+
             view.findViewById<TextView>(R.id.health_notes).text = goalString
             view.findViewById<TextView>(R.id.healthType).text = ""
             view.findViewById<TextView>(R.id.healthNum).text = ""
             view.findViewById<TextView>(R.id.healthUnits).text = ""
+
+            val editor = sharedpreferences!!.edit()
+            editor.putStringSet("health", typeList);
+            editor.commit()
         }
 
     }
--- app/src/main/java/com/example/practice_basicactivity/ToDoList.kt
+++ app/src/main/java/com/example/practice_basicactivity/ToDoList.kt
@@ -1,63 +1,88 @@
 package com.example.practice_basicactivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.SparseBooleanArray
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.ListView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.activity_main.*
 import androidx.navigation.fragment.findNavController
 
 
 class ToDoList : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
 
-        findViewById<Button>(R.id.ButtonToHome).setOnClickListener {
-            findNavController().navigate(R.id.action_Todo_to_Home)
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.todo_list, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.ButtonToHome).setOnClickListener {
+            findNavController().navigate(R.id.act_listToHome)
         }
 
         // Initializing the array lists and the adapter
         var itemlist = arrayListOf<String>()
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, itemlist)
+        var adapter = activity?.let {
+            ArrayAdapter<String>(it, android.R.layout.simple_list_item_multiple_choice, itemlist)}
+        val listView: ListView = view.findViewById(R.id.listView) as ListView
+        listView.setAdapter(adapter)
+
 
         // Adding the items to the list when the add button is pressed
-        findViewById<TextView>(R.id.add).setOnClickListener {
-            itemlist.add(findViewById<TextView>(R.id.editText).text.toString())
+        view.findViewById<TextView>(R.id.add).setOnClickListener {
+            itemlist.add(view.findViewById<TextView>(R.id.editText).text.toString())
             listView.adapter = adapter
-            adapter.notifyDataSetChanged()
+            if (adapter != null) {
+                adapter.notifyDataSetChanged()
+            }
             // This is because every time when you add the item the input space or the edit text space will be cleared
-            findViewById<TextView>(R.id.editText).setText("")
+            view.findViewById<TextView>(R.id.editText).setText("")
         }
 
         // Selecting and Deleting the items from the list when the delete button is pressed
-        findViewById<TextView>(R.id.delete).setOnClickListener {
+        view.findViewById<TextView>(R.id.delete).setOnClickListener {
             val position: SparseBooleanArray = listView.checkedItemPositions
             val count = listView.count
             var item = count - 1
             while (item >= 0) {
                 if (position.get(item)) {
-                    adapter.remove(itemlist.get(item))
+                    if (adapter != null) {
+                        adapter.remove(itemlist.get(item))
+                    }
                 }
                 item--
             }
             position.clear()
-            adapter.notifyDataSetChanged()
+            if (adapter != null) {
+                adapter.notifyDataSetChanged()
+            }
         }
 
         // Clearing all the items in the list when the clear button is pressed
-        findViewById<TextView>(R.id.clear).setOnClickListener {
+        view.findViewById<TextView>(R.id.clear).setOnClickListener {
             itemlist.clear()
-            adapter.notifyDataSetChanged()
+            if (adapter != null) {
+                adapter.notifyDataSetChanged()
+            }
         }
 
         // Adding the toast message to the list when an item on the list is pressed
         listView.setOnItemClickListener { adapterView, view, i, l ->
             android.widget.Toast.makeText(
-                this,
+                requireActivity(),
                 "You Selected the item --> " + itemlist.get(i),
                 android.widget.Toast.LENGTH_SHORT
             ).show()
